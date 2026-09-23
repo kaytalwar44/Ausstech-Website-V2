e6dba894eedd0e590448fc593734f8783fdf0c55
kaytalwar44 <kaytalwar44@gmail.com>
2026-09-23 21:18:13 +1000

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = path.dirname(
   fileURLToPath(import.meta.url)
 );
 export default defineConfig({
-  base: '/Ausstech-Website-V2/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
